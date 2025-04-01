8f5a3186d57155d59fbe891ba2cdc1ff00888c62
bentleylewis <bbentleylewis@gmail.com>
2025-04-01 14:49:12 -0500

    finish reset game function and button styling

--- rps.js
+++ rps.js
@@ -3,9 +3,11 @@ const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const buttons = document.querySelectorAll(".choices button");
 const choices = ["rock", "paper", "scissors"]
 let playerScore = 0;
 let computerScore = 0;
+let gameOver = false;
 
 
 function playRound(playerChoice) {
@@ -46,10 +48,31 @@ computerDisplay.textContent = `Computer: ${computerChoice}`;
 playerScoreDisplay.textContent = `Your score: ${playerScore}`;
 computerScoreDisplay.textContent = `Computer's score: ${computerScore}`;
 
+if (playerScore >= 5 || computerScore >= 5) {
+    resultDisplay.textContent = playerScore >= 5 ? "Game over! YOU WON!" : "Game over! YOU LOST!";
+    gameOver = true;
+    disableButtons(); 
+    }
+
 }
 
-function checkScore(playerScore, computerScore) {
-    if (playerScore >= 5|| computerScore >= 5) {
-        
-    }
+
+function disableButtons() {
+    buttons.forEach(button => button.disabled = true);
+}
+
+function enableButtons() {
+    buttons.forEach(button => button.disabled = false);
+}
+
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    gameOver = false;
+    resultDisplay.textContent = "Choose rock, paper, or scissors!";
+    playerDisplay.textContent = "";
+    computerDisplay.textContent = "";
+    playerScoreDisplay.textContent = "Your score: 0";
+    computerScoreDisplay.textContent = "Computer's score: 0";
+    enableButtons(); 
 }
